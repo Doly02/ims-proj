91197b233cb091307a20b043b55f954b6ca06add
xzahra33 <xzahra33@vutbr.cz>
2024-11-28 15:07:07 +0100

    [MODEL] Generator

--- include/GeneratorDay.hpp
+++ include/GeneratorDay.hpp
@@ -3,7 +3,7 @@
  *  File Name:      GenerateDay.cpp
  *  Authors:        Tomas Dolak, Monika Zahradnikova
  *  Date:           20.11.2024
- *  Description:    Implements Generator of The Day Time.
+ *  Description:    Implements Generator of The Day Time, Night Time or All Day Time.
  *
  * ****************************/
 
@@ -12,7 +12,7 @@
  *  @file           GeneratorDay.cpp
  *  @authors        Tomas Dolak, Monika Zahradnikova
  *  @date           20.11.2024
- *  @brief          Implements Generator of The Day Time.
+ *  @brief          Implements Generator of The Day Time, Night Time or All Day Time.
  * ****************************/
 
 #ifndef GENERATOR_DAY_HPP
@@ -39,6 +39,16 @@ extern int num_generated_cars;
  */
 extern bool model_active;
 
+
+/************************************************/
+/*             Generator Modes                  */
+/************************************************/
+enum GeneratorMode {
+    DAYTIME,    /** Generate transactions during the daytime. */
+    NIGHTTIME,  /** Generate transactions during the nighttime. */
+    ALLTIME     /** Generate transactions throughout the entire day. */
+};
+
 /************************************************/
 /*                   Classes                    */
 /************************************************/
@@ -49,7 +59,7 @@ extern bool model_active;
 class TransactionDay : public Process
 {
 private:
-    double day_length; /**< Duration of the daytime period. */
+    double day_length; /** Duration of the daytime period. */
 
 public:
     /**
@@ -71,20 +81,36 @@ public:
 class GeneratorDay : public Event
 {
 private:
-    int total_cars;            /** Total number of cars */
-    double charging_percentage; /** Percentage of cars charging */
-    double current_time; /** Current simulation time. */
+    int total_cars;                /** Total number of cars */
+    double charging_percentage;    /** Percentage of cars charging */
+    double current_time;           /** Current simulation time. */
+    GeneratorMode mode;            /** Mode of the generator (DAYTIME, NIGHTTIME, ALLTIME). */
 
 public:
     /**
-     * @brief Default constructor.
+     * @brief Constructor for GeneratorDay.
+     * @param total_cars Total number of cars.
+     * @param charging_percentage Percentage of cars charging daily.
+     * @param mode Generator mode (daytime, nighttime, or all-day).
      */
-    GeneratorDay(int total_cars, double charging_percentage);
+    GeneratorDay(int total_cars, double charging_percentage, GeneratorMode mode);
 
     /**
      * @brief Behavior function to manage the generation of EV transactions.
      */
     void Behavior() override;
 };
+
+/************************************************/
+/*             Helper Functions                 */
+/************************************************/
+/**
+ * @brief Calculate the generation rate (lambda) for the exponential distribution.
+ * @param total_cars Total number of cars.
+ * @param charging_percentage Percentage of cars to charge daily.
+ * @param period Time period for the simulation (in minutes).
+ * @return Lambda value for exponential distribution.
+ */
+double calculateGenerationLambda(double total_cars, double charging_percentage, double period);
 
 #endif /* GENERATOR_DAY_HPP */
