ba37486109eee7fbb51069ffc9f11500279658f7
xzahra33 <xzahra33@vutbr.cz>
2024-11-29 17:34:14 +0100

    [DOCS] Edit petri nets [MODEL] Edit constants in definitions.hpp

--- include/definitions.hpp
+++ include/definitions.hpp
@@ -92,42 +92,45 @@ typedef enum
 
 /**
  * @brief   Chance That Car Will Choose 11kWh AC Charging Station.
- * @details 
+ * @details 6 / 173 = 3.4%
  */
-#define CHANCE_11_KWH_AC_STATIONS            (0.1177)
+#define CHANCE_11_KWH_AC_STATIONS            (0.034)
 
 /**
  * @brief   Chance That Car Will Choose 12kWh AC Charging Station.
- * @details 12 / 102 = 11.77%
+ * @details 20 / 173 = 11.6%
  */
-#define CHANCE_12_KWH_AC_STATIONS           (CHANCE_11_KWH_AC_STATIONS + 0.1177)
+#define CHANCE_12_KWH_AC_STATIONS           (CHANCE_11_KWH_AC_STATIONS + 0.116)
 
 /**
  * @brief   Chance That Car Will Choose 22kWh AC Charging Station.
- * @details 58 / 102 = 56.86% 
+ * @details 77 / 173 = 44.5%
  */
-#define CHANCE_22_KWH_AC_STATIONS           (CHANCE_12_KWH_AC_STATIONS + 0.5686)
+#define CHANCE_22_KWH_AC_STATIONS           (CHANCE_12_KWH_AC_STATIONS + 0.445)
 
 /**
- * @brief   Chance That Car Will Choose 58kWh DC Charging Station.
- * @details 22 / 102 = 21.57% 
+ * @brief   Chance That Car Will Choose 50kWh DC Charging Station.
+ * @details 54 / 173 = 31.1%
  */
-#define CHANCE_50_KWH_DC_STATIONS           (CHANCE_22_KWH_AC_STATIONS + 0.2157)
+#define CHANCE_50_KWH_DC_STATIONS           (CHANCE_22_KWH_AC_STATIONS + 0.312)
 
 /**
  * @brief   Chance That Car Will Choose 108kWh DC Charging Station.
- * @details 10 / 102 = 9.80% 
+ * @details 11 / 173 = 6.3%
  */
-#define CHANCE_108_KWH_DC_STATIONS          (CHANCE_50_KWH_DC_STATIONS + 0.0980)
+#define CHANCE_108_KWH_DC_STATIONS          (CHANCE_50_KWH_DC_STATIONS + 0.063)
 
 /**
  * @brief   Chance That Car Will Choose 150kWh DC Charging Station.
- * @details 10 / 102 = 9.80% 
+ * @details 5 / 173 = 2.9%
  */
-#define CHANCE_150_KWH_DC_STATIONS          (CHANCE_108_KWH_DC_STATIONS + 0.0098)
+#define CHANCE_150_KWH_DC_STATIONS          (CHANCE_108_KWH_DC_STATIONS + 0.029)
 
-
+/**
+ * @brief Number of 11kWh AC Charging Stations in Brno.
+ */
 #define NUM_11_KWH_AC_STATIONS              (12)
+
 /**
  * @brief Number of 12kWh AC Charging Stations in Brno.
  */
@@ -146,18 +149,24 @@ typedef enum
 /**
  * @brief Number of 108kWh DC Charging Stations in Brno.
  */
-#define NUM_108_KWH_DC_STATIONS             (10)
+#define NUM_108_KWH_DC_STATIONS             (11)
 
-#define NUM_150_KWH_DC_STATIONS             (1)
+/**
+ * @brief Number of 150kWh DC Charging Stations in Brno.
+ */
+#define NUM_150_KWH_DC_STATIONS             (5)
 
+/**
+ * @brief Number Charging Stations in Brno.
+ */
 #define NUM_ALL_STATIONS (NUM_12_KWH_AC_STATIONS + NUM_22_KWH_AC_STATIONS + NUM_50_KWH_DC_STATIONS + NUM_108_KWH_DC_STATIONS + NUM_150_KWH_DC_STATIONS)
 
 /**
- * @brief   Max. Time For Charging on AC 12 kWh Charger,
+ * @brief   Max. Time For Charging on AC 11 kWh Charger,
  *          In First Stage of Charging (0% - 20%).
- * @details 1.19 hours => 71.4 minutes
+ * @details 1.30 hours => 78 minutes
  */
-#define MAX_TIME_11_KWH_AC_STATIONS_0_20     (71.4)
+#define MAX_TIME_11_KWH_AC_STATIONS_0_20     (78)
 /**
  * @brief   Max. Time For Charging on AC 12 kWh Charger,
  *          In First Stage of Charging (0% - 20%).
@@ -189,91 +198,91 @@ typedef enum
 /**
  * @brief   Max. Time For Charging on DC 150 kWh Charger,
  *          In First Stage of Charging (0% - 20%).
- * @details 0.26 hours => 15.6 minutes
+ * @details 0.19 hours => 11.4 minutes
  */
-#define MAX_TIME_150_KWH_DC_STATIONS_0_20    (15.6) // TODO:
+#define MAX_TIME_150_KWH_DC_STATIONS_0_20    (11.4)
 
 /**
- * @brief   Max. Time For Charging on AC 12 kWh Charger,
- *          In First Stage of Charging (20% - 80%).
- * @details 3.58 hours => 214.8 minutes
+ * @brief   Max. Time For Charging on AC 11 kWh Charger,
+ *          In Second Stage of Charging (20% - 80%).
+ * @details 3.90 hours => 234 minutes
  */
-#define MAX_TIME_11_KWH_AC_STATIONS_20_80    (214.8) // TODO:    
+#define MAX_TIME_11_KWH_AC_STATIONS_20_80    (234)
 /**
  * @brief   Max. Time For Charging on AC 12 kWh Charger,
- *          In First Stage of Charging (20% - 80%).
+ *          In Second Stage of Charging (20% - 80%).
  * @details 3.58 hours => 214.8 minutes
  */
 #define MAX_TIME_12_KWH_AC_STATIONS_20_80    (214.8)
 
 /**
  * @brief   Max. Time For Charging on AC 22 kWh Charger,
- *          In First Stage of Charging (20% - 80%).
+ *          In Second Stage of Charging (20% - 80%).
  * @details 1.95 hours => 117 minutes
  */
 #define MAX_TIME_22_KWH_AC_STATIONS_20_80    (117)
 
 /**
  * @brief   Max. Time For Charging on DC 50 kWh Charger,
- *          In First Stage of Charging (20% - 80%).
+ *          In Second Stage of Charging (20% - 80%).
  * @details 1.01 hours => 60.6 minutes
  */
 #define MAX_TIME_50_KWH_DC_STATIONS_20_80    (60.6)
 
 /**
  * @brief   Max. Time For Charging on DC 108 kWh Charger,
- *          In First Stage of Charging (20% - 80%).
+ *          In Second Stage of Charging (20% - 80%).
  * @details 0.47 hours => 28.2 minutes
  */
 #define MAX_TIME_108_KWH_DC_STATIONS_20_80   (28.2)
 
 /**
  * @brief   Max. Time For Charging on DC 150 kWh Charger,
- *          In First Stage of Charging (20% - 80%).
- * @details 0.47 hours => 28.2 minutes
+ *          In Second Stage of Charging (20% - 80%).
+ * @details 0.34 hours => 20.4 minutes
  */
-#define MAX_TIME_150_KWH_DC_STATIONS_20_80   (28.2)     // TODO:
+#define MAX_TIME_150_KWH_DC_STATIONS_20_80   (20.4)
 
 /**
  * @brief   Max. Time For Charging on AC 11 kWh Charger,
- *          In First Stage of Charging (80% - 100%).
- * @details 2.38 hours => 142.8 minutes
+ *          In Third Stage of Charging (80% - 100%).
+ * @details 2.60 hours => 156 minutes
  */
-#define MAX_TIME_11_KWH_AC_STATIONS_80_100   (142.8)    // TODO:
+#define MAX_TIME_11_KWH_AC_STATIONS_80_100   (156)
 
 /**
  * @brief   Max. Time For Charging on AC 12 kWh Charger,
- *          In First Stage of Charging (80% - 100%).
+ *          In Third Stage of Charging (80% - 100%).
  * @details 2.38 hours => 142.8 minutes
  */
 #define MAX_TIME_12_KWH_AC_STATIONS_80_100   (142.8)
 
 /**
  * @brief   Max. Time For Charging on AC 22 kWh Charger,
- *          In First Stage of Charging (80% - 100%).
+ *          In Third Stage of Charging (80% - 100%).
  * @details 1.30 hours => 78 minutes
  */
 #define MAX_TIME_22_KWH_AC_STATIONS_80_100   (78)
 
 /**
  * @brief   Max. Time For Charging on DC 50 kWh Charger,
- *          In First Stage of Charging (80% - 100%).
+ *          In Third Stage of Charging (80% - 100%).
  * @details 0.82 hours => 49.2 minutes
  */
 #define MAX_TIME_50_KWH_DC_STATIONS_80_100   (49.2)
 
 /**
  * @brief   Max. Time For Charging on DC 108 kWh Charger,
- *          In First Stage of Charging (80% - 100%).
+ *          In Third Stage of Charging (80% - 100%).
  * @details 0.38 hours => 22.8 minutes
  */
 #define MAX_TIME_108_KWH_DC_STATIONS_80_100  (22.8)
 
 /**
  * @brief   Max. Time For Charging on DC 108 kWh Charger,
- *          In First Stage of Charging (80% - 100%).
- * @details 0.38 hours => 22.8 minutes
+ *          In Third Stage of Charging (80% - 100%).
+ * @details 0.27 hours => 16.2 minutes
  */
-#define MAX_TIME_150_KWH_DC_STATIONS_80_100  (22.8) // TODO:
+#define MAX_TIME_150_KWH_DC_STATIONS_80_100  (16.2)
 
 #endif // DEFINITIONS_HPP
